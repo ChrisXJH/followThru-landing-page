2ef647a0ca7a610b3b1cd426ca259bad9c1d3c15
Xu <gzchrisxjh@gmail.com>
2017-08-17 15:18:36 -0400

    Removed unnecessary files and codes

--- js/page.class.js
+++ js/page.class.js
@@ -1,9 +1,6 @@
 function Page(elem, components) {
   this.elem = elem;
   this.id = elem.attr("id");
-  // this.elem.height(this.elem.height());
-  this.components = [];
-  this.initComponents();
   this.bind_events();
 }
 
@@ -15,12 +12,4 @@ Page.prototype.bind_events = function () {
       self.elem.addClass("show-components");
     }
   });
-};
-
-Page.prototype.initComponents = function () {
-  var components = this.elem.find(".component");
-  for (var i = 0; i < components.length; i++) {
-    var cpn = new Component(components.eq(i));
-    this.components.push(cpn);
-  }
 };
